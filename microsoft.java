713ce0b1a9ba08dfc6b6bd4512443363fb87fb46
Rohan Kumar <152252631+heyIamRohan@users.noreply.github.com>
2024-01-23 14:47:35 +0530

    Add files via upload

---
+++ microsoft.java
@@ -0,0 +1,37 @@
+class microsoft
+
+{
+	void version()
+	
+	{
+		System.out.println("successfully install windows");
+	}
+}
+	class hp
+	{
+		static void install (microsoft ref)
+		{
+			ref.version();
+		}
+	}
+	class dell
+	{
+		static void install(microsoft ref)
+		{
+			ref.version();
+		}
+
+	}
+    class win8
+    {
+    } 
+    {
+    
+	
+public static void main(String[] args)
+{
+	microsoft w=new microsoft();
+	      hp.install(w);
+		  dell.microsoft(w);
+}
+}
